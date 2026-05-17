2879a91a997d8d2268c70d75f4be003c0d9ccc8a
lee <lee9choco@gmail.com>
2026-05-17 09:56:42 +0900

    💄 style: Round player chips and center content

--- lib/features/doubles_scheduler/presentation/widgets/schedule_player_chip.dart
+++ lib/features/doubles_scheduler/presentation/widgets/schedule_player_chip.dart
@@ -65,7 +65,7 @@ class SchedulePlayerChip extends StatelessWidget {
   Widget build(BuildContext context) {
     final playerId = this.playerId;
     final colorScheme = Theme.of(context).colorScheme;
-    final borderRadius = BorderRadius.circular(14);
+    final borderRadius = BorderRadius.circular(20);
     final backgroundColor =
         isHighlighted ? colorScheme.tertiaryContainer : colorScheme.surface;
     final borderColor =
@@ -75,29 +75,32 @@ class SchedulePlayerChip extends StatelessWidget {
     final chipContent = SizedBox(
       width: _chipWidth,
       height: _chipHeight,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ScheduleSlotIcon(
-            slotNumber: slotNumber,
-            playerId: playerId,
-            displayName: displayName,
-            size: _iconSize,
-          ),
-          const SizedBox(height: 1),
-          Text(
-            displayName,
-            maxLines: 1,
-            overflow: TextOverflow.ellipsis,
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: _nameFontSize,
-              height: 1.1,
-              fontWeight: isHighlighted ? FontWeight.w700 : FontWeight.normal,
-              color: isHighlighted ? colorScheme.onTertiaryContainer : null,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ScheduleSlotIcon(
+              slotNumber: slotNumber,
+              playerId: playerId,
+              displayName: displayName,
+              size: _iconSize,
             ),
-          ),
-        ],
+            const SizedBox(height: 1),
+            Text(
+              displayName,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: _nameFontSize,
+                height: 1.1,
+                fontWeight: isHighlighted ? FontWeight.w700 : FontWeight.normal,
+                color: isHighlighted ? colorScheme.onTertiaryContainer : null,
+              ),
+            ),
+          ],
+        ),
       ),
     );
 
