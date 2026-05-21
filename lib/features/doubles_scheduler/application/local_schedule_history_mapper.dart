ea3ce6da2e92adb4a3b9699d68c3eb79ba5eefb9
lee <lee9choco@gmail.com>
2026-05-21 20:03:55 +0900

    ♻️ refactor: map history player count

--- lib/features/doubles_scheduler/application/local_schedule_history_mapper.dart
+++ lib/features/doubles_scheduler/application/local_schedule_history_mapper.dart
@@ -9,7 +9,7 @@ LocalScheduleHistoryItem buildLocalScheduleHistoryItem(
     publicId: aggregate.event.publicId,
     title: aggregate.event.title,
     courtCount: aggregate.event.courtCount,
-    participantCount: aggregate.participants.length,
+    playerCount: aggregate.participants.length,
     firstSavedAt: now,
     lastOpenedAt: now,
   );
