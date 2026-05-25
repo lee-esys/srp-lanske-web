e8e53f934c9e1ecebd6d869cc9f41361c257d3b3
lee <lee9choco@gmail.com>
2026-05-25 22:07:38 +0900

    ✨ feat: support same-tab URL navigation refs #67

--- lib/shared/utils/external_link_web.dart
+++ lib/shared/utils/external_link_web.dart
@@ -3,3 +3,7 @@ import 'package:web/web.dart' as web;
 void openExternalUrl(String url) {
   web.window.open(url, '_blank', 'noopener,noreferrer');
 }
+
+void openUrlInCurrentTab(String url) {
+  web.window.location.href = url;
+}
