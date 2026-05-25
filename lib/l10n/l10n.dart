f30a8c24463f50b44ea3036ac960eb2ea55a0b82
lee <lee9choco@gmail.com>
2026-05-25 09:16:27 +0900

    🌐 l10n: add share dialog labels refs #60

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,1 +1,7 @@
 export 'app_localizations.dart';
+
+extension ShareUrlLocalizations on AppLocalizations {
+  String get shareUrlButton => localeName == 'en' ? 'Share URL' : 'URLを共有';
+
+  String get closeButton => localeName == 'en' ? 'Close' : '閉じる';
+}
