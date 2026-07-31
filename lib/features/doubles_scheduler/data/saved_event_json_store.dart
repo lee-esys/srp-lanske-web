a19937a4545cc61ecbbe167ec6c9ab82e82e9137
lee <lee9choco@gmail.com>
2026-08-01 07:08:12 +0900

    🧱 feat: add atomic JSON update contract refs #153

--- lib/features/doubles_scheduler/data/saved_event_json_store.dart
+++ lib/features/doubles_scheduler/data/saved_event_json_store.dart
@@ -1,3 +1,26 @@
+typedef SavedEventJsonUpdater = SavedEventJsonUpdate Function(
+  Map<String, dynamic> current,
+);
+
+class SavedEventJsonUpdate {
+  const SavedEventJsonUpdate({
+    required this.data,
+    required this.fields,
+  });
+
+  factory SavedEventJsonUpdate.noOp(Map<String, dynamic> data) {
+    return SavedEventJsonUpdate(
+      data: data,
+      fields: const <String, dynamic>{},
+    );
+  }
+
+  final Map<String, dynamic> data;
+  final Map<String, dynamic> fields;
+
+  bool get isNoOp => fields.isEmpty;
+}
+
 abstract class SavedEventJsonStore {
   Future<void> saveByPublicId({
     required String publicId,
@@ -7,4 +30,11 @@ abstract class SavedEventJsonStore {
   Future<Map<String, dynamic>?> findByPublicId(String publicId);
 
   Future<Map<String, dynamic>?> findByEventId(String eventId);
+
+  Future<Map<String, dynamic>?> updateByPublicId({
+    required String publicId,
+    required SavedEventJsonUpdater update,
+  }) {
+    throw UnimplementedError('updateByPublicId is not implemented');
+  }
 }
