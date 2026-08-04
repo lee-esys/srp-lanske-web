0bef54569b0d193a3a167206ff65fcd7a75225fd
lee <lee9choco@gmail.com>
2026-08-04 11:48:53 +0900

    ♻️ refactor: move app footer visibility into shared host refs #167

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -6,11 +6,33 @@ import '../features/doubles_scheduler/presentation/restored_schedule_page.dart';
 import '../features/team_scheduler/presentation/team_schedule_list_page.dart';
 import '../features/team_scheduler/presentation/team_schedule_page.dart';
 import '../features/team_scheduler/presentation/team_setup_page.dart';
-import '../shared/presentation/app_footer.dart';
+import '../shared/presentation/app_footer_host.dart';
 import 'theme/app_theme.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  late final AppFooterResetController _footerResetController;
+  late final AppFooterNavigatorObserver _footerNavigatorObserver;
+
+  @override
+  void initState() {
+    super.initState();
+    _footerResetController = AppFooterResetController();
+    _footerNavigatorObserver =
+        AppFooterNavigatorObserver(_footerResetController);
+  }
+
+  @override
+  void dispose() {
+    _footerResetController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,12 +50,11 @@ class App extends StatelessWidget {
       supportedLocales: AppLocalizations.supportedLocales,
       locale: const Locale('ja'),
       theme: appTheme,
+      navigatorObservers: [_footerNavigatorObserver],
       builder: (context, child) {
-        return Column(
-          children: [
-            Expanded(child: child ?? const SizedBox.shrink()),
-            const AppFooter(),
-          ],
+        return AppFooterHost(
+          resetListenable: _footerResetController,
+          child: child ?? const SizedBox.shrink(),
         );
       },
       home: home,
