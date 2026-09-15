055ee243d803eb33671b2ff43bc825ab6a727e42
lee <lee9choco@gmail.com>
2026-09-15 11:29:32 +0900

    🧪 fix: avoid broken Firestore emulator clear endpoint refs #213

--- test/firestore_rules/external_identity_rules.test.js
+++ test/firestore_rules/external_identity_rules.test.js
@@ -8,6 +8,7 @@ const {
   assertSucceeds,
   initializeTestEnvironment,
 } = require('@firebase/rules-unit-testing');
+const { clearFirestoreCollections } = require('./test_environment');
 
 const projectId = 'demo-lanske-rules';
 const sourceType = 'tennisbear';
@@ -21,15 +22,11 @@ let testEnv;
 before(async () => {
   testEnv = await initializeTestEnvironment({
     projectId,
-    firestore: {
-      host: '127.0.0.1',
-      port: 8080,
-    },
   });
 });
 
 beforeEach(async () => {
-  await testEnv.clearFirestore();
+  await clearFirestoreCollections(testEnv);
 });
 
 after(async () => {
