3f2237c5f3d108a470ea6c95d97ae0bae183fc2c
lee <lee9choco@gmail.com>
2026-09-24 12:09:01 +0900

    ♻️ refactor: make event owner preservation explicit refs #224

--- lib/features/doubles_scheduler/domain/saved_event_models.dart
+++ lib/features/doubles_scheduler/domain/saved_event_models.dart
@@ -100,7 +100,7 @@ class SavedEvent {
     return SavedEvent(
       id: id,
       publicId: publicId,
-      ownerUid: ownerUid,
+      ownerUid: this.ownerUid,
       title: title ?? this.title,
       memo: memo ?? this.memo,
       eventDate: eventDate ?? this.eventDate,
